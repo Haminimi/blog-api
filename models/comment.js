73ce52c02c0cc4c28113e1b3799493beab925ebb
Hamza Mujinovic <haminimi.dev@gmail.com>
2024-02-16 00:57:52 +0100

    Initialize the comment schema

---
+++ models/comment.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+	username: String,
+	comment: { type: String, required: true },
+	post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
+	timestamp: { type: Number, default: Date.now, required: true },
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
